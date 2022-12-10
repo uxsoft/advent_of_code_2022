a6d4cc6ceecdb4cef94b2ce22855babb49945107
Jan Dryk <me@uxsoft.cz>
2022-12-10 17:47:19 +0100

    day 6

--- src/day_6.rs
+++ src/day_6.rs
@@ -1,12 +1,52 @@
-﻿
+﻿use std::collections::LinkedList;
 
-pub fn index_of_unique_chars(n: u32, input: String)
-{
+pub fn index_of_unique_chars(n: usize, input: &str) -> usize {
+    let chars = input.as_bytes();
+    let mut buffer: LinkedList<u8> = LinkedList::new(); // Last n unique characters
+    let mut i = 0;
+    let mut n_unique = 0;
 
+    loop {
+        let current_char = chars.get(i).unwrap();
+
+        // If we encounter a duplicate keep removing until we remove the duplicate
+        while buffer.contains(current_char) {
+            // Reduce buffer by 1 char from the back
+            buffer.pop_back();
+            n_unique = n_unique - 1;
+        } 
+        
+        // Add current char
+        n_unique = n_unique + 1;
+        buffer.push_front(*current_char);
+
+        
+        
+        // if buffer.len() > n {
+        //     buffer.pop_back();
+        // }
+
+        // If we have 4 unique chars we succeeded and we can return
+        // I has index of the last character, we need to subtract 4?
+        // If not move on
+        {
+            // DEBUG
+            let processed_str : &str = &input.to_string()[0..i+1];
+            println!("[{}] {} {}", i, n_unique, processed_str);   
+        }
+        if n_unique == n {
+            break (i + 1);
+        } 
+        i = i + 1;
+    }
 }
 
 pub fn process(input: String) {
-    input.
+    let i = index_of_unique_chars(4, input.as_str());
+    println!("First 4 unique chars appear at {}", i);
+
+    let j = index_of_unique_chars(14, input.as_str());
+    println!("First 14 unique chars appear at {}", j);
 }
 
 #[cfg(test)]
@@ -15,7 +55,48 @@ mod tests_part6 {
     use super::*;
 
     #[test]
-    fn test_examples() {
-        assert_
+    fn test_n4() {
+        assert_eq!(
+            index_of_unique_chars(4, "mjqjpqmgbljsphdztnvjfqwrcgsmlb"),
+            7
+        );
+        assert_eq!(
+            index_of_unique_chars(4, "nppdvjthqldpwncqszvftbrmjlhg"), 
+            6
+        );
+        assert_eq!(
+            index_of_unique_chars(4, "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"),
+            10
+        );
+        assert_eq!(
+            index_of_unique_chars(4, "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"),
+            11
+        );
+        assert_eq!(
+            index_of_unique_chars(4, "bvwbjplbgvbhsrlpgdmjqwftvncz"), 
+            5
+        );
+    }
+    fn test_n14() {
+        assert_eq!(
+            index_of_unique_chars(4, "mjqjpqmgbljsphdztnvjfqwrcgsmlb"),
+            19
+        );
+        assert_eq!(
+            index_of_unique_chars(4, "nppdvjthqldpwncqszvftbrmjlhg"), 
+            23
+        );
+        assert_eq!(
+            index_of_unique_chars(4, "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"),
+            23
+        );
+        assert_eq!(
+            index_of_unique_chars(4, "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"),
+            29
+        );
+        assert_eq!(
+            index_of_unique_chars(4, "bvwbjplbgvbhsrlpgdmjqwftvncz"), 
+            26
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,11 @@ mod day_6;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let default_filename = "day6_dry.txt".to_string();
+    let default_filename = "day6_sharp.txt".to_string();
     let filename = args.get(1).unwrap_or(&default_filename);
     
     let input = fs::read_to_string(format!("data\\{}", filename))
         .expect("Wrong file location");
     
-    day6::process(input);
+    day_6::process(input);
 }
