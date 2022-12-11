7c11c4746b7a3bf9c3c0d5e6c8b7254c5d842069
Jan Dryk <me@uxsoft.cz>
2022-12-11 23:59:01 +0100

    day8_1

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,11 @@ mod day8;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let default_filename = "day7_sharp.txt".to_string();
+    let default_filename = "day8_sharp.txt".to_string();
     let filename = args.get(1).unwrap_or(&default_filename);
     
     let input = fs::read_to_string(format!("data\\{}", filename))
         .expect("Wrong file location");
     
-    day7_2::process(input);
+    day8::process(input);
 }
