5afe898feffcbeec68e278d95f43abf924024662
Jan Dryk <jan.dryk@merck.com>
2022-12-11 13:05:17 +0100

    day7 continued

--- src/day7.rs
+++ src/day7.rs
@@ -1,39 +1,36 @@
-use std::collections::HashMap;
+use std::{
+    cell::RefCell,
+    collections::HashMap,
+    rc::{Rc, Weak},
+};
 
-pub struct IdGenerator {
-    previous_id: usize
-}
-
-impl IdGenerator {
-    pub fn new() -> IdGenerator {
-        IdGenerator {
-            previous_id: 0
-        }
-    }
-    
-    pub fn new_id(&mut self) -> usize {
-        self.previous_id += 1;
-        self.previous_id
-    }
-}
-
-pub struct ElFSNode {
-    id: usize,
+pub struct Node {
     name: String,
     files: HashMap<String, u32>,
-    parent: Option<usize>,
-    directories: Vec<usize>
+    parent: Option<Weak<RefCell<Node>>>,
+    directories: Vec<Rc<RefCell<Node>>>,
 }
 
-impl ElFSNode {
-    pub fn new(id: usize, name: String, parent: Option<usize>) -> ElFSNode {
-        ElFSNode {
-            id,
+type NodePtr = Rc<RefCell<Node>>;
+
+pub struct Tree {
+    root: NodePtr
+}
+
+impl Node {
+    pub fn new(name: String, parent: Option<NodePtr>) -> Node {
+        Node {
             name,
-            parent,
+            parent: parent.map(|x| Rc::downgrade(&x)),
             files: HashMap::new(),
             directories: Vec::new(),
         }
+    }
+}
+
+impl From<Node> for NodePtr {
+    fn from(node: Node) -> NodePtr {
+        Rc::new(RefCell::new(node))
     }
 }
 
@@ -66,24 +63,20 @@ impl ElFSCommand {
 
 pub fn process(input: String) {
     let commands: Vec<&str> = input.split("$").skip(1).collect();
-    let mut idgen = IdGenerator::new();
-    let mut nodes = HashMap::new();
-    
-    let root = ElFSNode::new(idgen.new_id(), "root".to_string(), None);
-    nodes.insert(root.id, root);
-    
-    let mut current_dir = &root.id;
+
+    let root = Node::new("root".to_string(), None);
+    let mut current_dir : NodePtr = root.into();
 
     for cmd in commands {
         let command = ElFSCommand::parse(cmd).expect("Could not parse the command");
         match command {
             ElFSCommand::CdRoot => {
-                current_dir = &root.id;
+                current_dir = root.into();
                 println!("Changed dir to root");
             }
             ElFSCommand::CdUp => {
-                let parent_id = nodes.get(current_dir).unwrap().parent.expect("Can't go up when there is no parent");
-                current_dir = &parent_id;
+                let parent = current_dir.borrow().parent.expect("This directory has no parent").upgrade().unwrap();
+                current_dir = parent;
             }
             ElFSCommand::Cd(target_dir_name) => {
                 let child = nodes.get(current_dir).unwrap().directories.iter()
@@ -95,9 +88,12 @@ pub fn process(input: String) {
                 for line in lsoutput.lines() {
                     if line.starts_with("dir") {
                         let dir_name = line.trim_start_matches("dir ");
-                        let new_node = ElFSNode::new(idgen.new_id(), "dir_name".to_string(), Some(*current_dir));
+                        let new_node = Node::new(
+                            idgen.new_id(),
+                            "dir_name".to_string(),
+                            Some(*current_dir),
+                        );
                         nodes.insert(new_node.id, new_node);
-                        
                     }
                 }
             }
